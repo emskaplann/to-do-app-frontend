e19df56e5dd5efb5f56c0be617888a97255f01f3
rmeji1 <rmeji1@unh.newhaven.edu>
2019-12-03 12:33:22 -0500

    add a tag component

--- src/components/main-components/Projects.js
+++ src/components/main-components/Projects.js
@@ -1,75 +1,76 @@
 import React from 'react';
-import {Container, Col, Row, Card, ListGroup} from 'react-bootstrap/';
+import { Container, Col, Row, Card, ListGroup } from 'react-bootstrap/';
+import TagSerivce from '../../services/TagService';
+import TagComponent from '../sub-components/TagComponent';
 
 export default class Projects extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-
+      tags: []
     }
+    this.tagService = new TagSerivce(this)
   }
-  render(){
-    return(
+
+  componentDidMount() {
+    this.tagService.fetchAll()
+  }
+
+  render() {
+    return (
       <Container fluid>
-    <Row>
-      <Col sm={7}>
-        <Card>
-          <Card.Header style={{ backgroundColor: '#0099FF', color: "#fff" }}>
-            My Pro
+        <Row>
+          <Col sm={7}>
+            <Card>
+              <Card.Header style={{ backgroundColor: '#0099FF', color: "#fff" }}>
+                My Pro
           </Card.Header>
-          <Card.Body>
-            Desc
+              <Card.Body>
+                Desc
           </Card.Body>
-          <Card.Body style={{border: '1px solid #d3d3d3'}}>
-            <Row>
-            <Col sm={1.5}>
-              <ListGroup.Item style={{marginRight: 5}}>Tags:</ListGroup.Item>
-            </Col>
-              <ListGroup>
-                <ListGroup.Item style={{backgroundColor: '#d4c1c2', color: "#fff", border: '0.5px solid #d4c1c2', fontWeight: 'bold'}}>asdasd</ListGroup.Item>
-              </ListGroup>
-            </Row>
-          </Card.Body>
-        </Card>
-        <br />
-          <Card>
-            <Card.Header style={{backgroundColor: '#990033', color: '#fff'}}>
-              Notes
+              <Card.Body style={{ border: '1px solid #d3d3d3' }}>
+                <TagComponent tags={this.state.tags} />
+              </Card.Body>
+            </Card>
+            <br />
+            <Card>
+              <Card.Header style={{ backgroundColor: '#990033', color: '#fff' }}>
+                Notes
             </Card.Header>
-            <Card.Body>
-              <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Cras justo odio</ListGroup.Item>
-              <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Dapibus ac facilisis in</ListGroup.Item>
-              <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Vestibulum at eros</ListGroup.Item>
-              <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Cras justo odio</ListGroup.Item>
-            </Card.Body>
-          </Card>
-      </Col>
-      <Col sm={5}>
-        <Card>
-          <Card.Header style={{ backgroundColor: '#0033CC', color: "#fff" }}>
-            Tasks
+              <Card.Body>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Vestibulum at eros</ListGroup.Item>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Cras justo odio</ListGroup.Item>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={5}>
+            <Card>
+              <Card.Header style={{ backgroundColor: '#0033CC', color: "#fff" }}>
+                Tasks
           </Card.Header>
-          <Card.Body>
-            <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Vestibulum at eros</ListGroup.Item>
-            <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Cras justo odio</ListGroup.Item>
-          </Card.Body>
-        </Card>
-        <br />
-          <Card>
-            <Card.Header style={{ backgroundColor: '#669900', color: "#fff" }}>
-              Completed Tasks
+              <Card.Body>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Vestibulum at eros</ListGroup.Item>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Cras justo odio</ListGroup.Item>
+              </Card.Body>
+            </Card>
+            <br />
+            <Card>
+              <Card.Header style={{ backgroundColor: '#669900', color: "#fff" }}>
+                Completed Tasks
             </Card.Header>
-            <Card.Body>
-              <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Cras justo odio</ListGroup.Item>
-              <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Dapibus ac facilisis in</ListGroup.Item>
-              <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Vestibulum at eros</ListGroup.Item>
-              <ListGroup.Item style={{border: '1px solid #d3d3d3'}}>Cras justo odio</ListGroup.Item>
-            </Card.Body>
-          </Card>
-      </Col>
-    </Row>
+              <Card.Body>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Vestibulum at eros</ListGroup.Item>
+                <ListGroup.Item style={{ border: '1px solid #d3d3d3' }}>Cras justo odio</ListGroup.Item>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     )
   }
--- src/components/sub-components/UpcomingTasks.js
+++ src/components/sub-components/UpcomingTasks.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Card, ListGroup } from 'react-bootstrap'
 const UpcomingTasks = ({ tasks }) => {
-  tasks.filter(task => new Date(task.deadline))
+  const todaysDate = new Date().setHours(0, 0, 0, 0)
+  const upcomingTasks = tasks.filter(task => new Date(task.deadline).valueOf() <= todaysDate.valueOf())
+  console.log(upcomingTasks)
   return (
     <div>
       <Card style={{ width: '850px', maxWidth: '100%' }}>
         <Card.Header style={{ backgroundColor: '#0033CC', color: "#fff" }}>
           Upcoming Tasks <i className="fa fa-fw fa-caret-up" style={{ fontSize: '1em', marginLeft: 5 }} />
         </Card.Header>
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        {upcomingTasks.map(task => <ListGroup.Item className='d-flex' key={`list-group-item-${task.id}`}>{task.title}<span className='flex-fill text-right'>Due today!</span></ListGroup.Item>)}
       </Card>
     </div>
   )

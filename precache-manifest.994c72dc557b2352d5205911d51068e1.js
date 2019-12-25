c7e999f16ad74f7caf758a1a429f822fd81c1c5e
Emirhan Kaplan <emskaplann@gmail.com>
2019-12-24 21:47:28 -0500

    Updates

---
+++ precache-manifest.994c72dc557b2352d5205911d51068e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa95275de25b438c12bd3b1a95fcde18",
+    "url": "/to-do-app-frontend/index.html"
+  },
+  {
+    "revision": "7a9dc14e5f226947b26e",
+    "url": "/to-do-app-frontend/static/css/2.f06938a8.chunk.css"
+  },
+  {
+    "revision": "3575753711918f911a2c",
+    "url": "/to-do-app-frontend/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7a9dc14e5f226947b26e",
+    "url": "/to-do-app-frontend/static/js/2.5e6c09c4.chunk.js"
+  },
+  {
+    "revision": "3575753711918f911a2c",
+    "url": "/to-do-app-frontend/static/js/main.b4e3416c.chunk.js"
+  },
+  {
+    "revision": "a9c25e0bc21935e895f1",
+    "url": "/to-do-app-frontend/static/js/runtime-main.be368c87.js"
+  }
+]);

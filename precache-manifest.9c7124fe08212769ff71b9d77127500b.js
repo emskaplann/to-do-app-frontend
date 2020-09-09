68595bebdabe258bd1875a225276198b7603a635
Emirhan Kaplan <emskaplann@gmail.com>
2020-09-09 01:08:03 -0400

    Updates

--- precache-manifest.9c7124fe08212769ff71b9d77127500b.js
+++ precache-manifest.9c7124fe08212769ff71b9d77127500b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c2da33665627465a1ceb4308c84c5bd6",
+    "revision": "7520f0023904f0a7d2c4e5a0334c730b",
     "url": "/to-do-app-frontend/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/to-do-app-frontend/static/css/2.4cbce5c2.chunk.css"
   },
   {
-    "revision": "ff438c15621f5dbd352e",
+    "revision": "a272ec1a8594fd188760",
     "url": "/to-do-app-frontend/static/css/main.34de6062.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/to-do-app-frontend/static/js/2.7afff370.chunk.js"
   },
   {
-    "revision": "ff438c15621f5dbd352e",
-    "url": "/to-do-app-frontend/static/js/main.23eac4de.chunk.js"
+    "revision": "a272ec1a8594fd188760",
+    "url": "/to-do-app-frontend/static/js/main.710808b0.chunk.js"
   },
   {
     "revision": "a9c25e0bc21935e895f1",
